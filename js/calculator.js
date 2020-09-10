498fcae9bd2b55d4d2132dced434c4d52a4bc0ba
Sun Jia Cheng <sunjc826@gmail.com>
2020-09-10 15:12:40 +0800

    Add basic operations and html layout

---
+++ js/calculator.js
@@ -0,0 +1,23 @@
+// basic arithmetic functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// generic operate function
+
+function operate(fn, a, b) {
+    return fn(a, b);
+}
